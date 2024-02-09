65e0b0b9ed7614dbc64f219daee70c7beb091450
Santanu Biswas <shanpro.2015@gmail.com>
2024-02-09 08:57:27 +0530

    introduce app.ts

---
+++ auth/src/app.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+import cookieSession from 'cookie-session';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(express.json());
+
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all('*', async (req, res, next) => {
+  next(new NotFoundError());
+});
+
+app.use(errorHandler);
+
+export { app };
