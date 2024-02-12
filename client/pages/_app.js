08c4d8cd586a08d0200dea2a1ee38c22ec14b979
Santanu Biswas <shanpro.2015@gmail.com>
2024-02-12 21:06:22 +0530

    Client - next app

---
+++ client/pages/_app.js
@@ -0,0 +1,37 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
+import Header from '../components/header';
+
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <Component {...pageProps} />
+    </div>
+  );
+};
+
+AppComponent.getInitialProps = async appContext => {
+  const client = buildClient(appContext.ctx);
+  let currentUserData = {};
+  try {
+    const { data } = await client.get('/api/users/currentuser'); 
+    currentUserData = data;
+  } catch (error) {
+    console.log('Not autheticated');
+  }
+  
+
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  return {
+    pageProps,
+    // ...data
+    ...currentUserData
+  };
+};
+
+export default AppComponent;
