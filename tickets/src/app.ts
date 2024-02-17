470f110c648d3531703e19fae852ce9de09c28af
Santanu Biswas <shanpro.2015@gmail.com>
2024-02-17 22:50:18 +0530

    Add tickets serice

---
+++ tickets/src/app.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+
+import { errorHandler, NotFoundError, currentUser } from '@sbticketingudemy/ticketing-udemy-common';
+import cookieSession from 'cookie-session';
+
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(express.json());
+
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
+
+app.all('*', async (req, res, next) => {
+  next(new NotFoundError());
+});
+
+app.use(errorHandler);
+
+export { app };
