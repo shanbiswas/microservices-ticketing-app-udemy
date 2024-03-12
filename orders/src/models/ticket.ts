f00a13207c50422902c3558ceb10ad602ce8a497
Santanu Biswas <shanpro.2015@gmail.com>
2024-03-12 18:16:01 +0530

    Implement version in events

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -1,5 +1,6 @@
-import mongoose from 'mongoose';
+import mongoose, { version } from 'mongoose';
 import { Order, OrderStatus } from './order';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 
 interface TicketAttrs {
   id: string;
@@ -15,6 +16,7 @@ export interface TicketDoc extends mongoose.Document {
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
   build(attrs: TicketAttrs): TicketDoc;
+  findByEvent(event: { id: string, version: number }): Promise<TicketDoc | null>;
 }
 
 const ticketSchema = new mongoose.Schema(
@@ -39,6 +41,9 @@ const ticketSchema = new mongoose.Schema(
   }
 );
 
+ticketSchema.set('versionKey', 'version');
+ticketSchema.plugin(updateIfCurrentPlugin);
+
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket({
     _id: attrs.id,
@@ -46,6 +51,14 @@ ticketSchema.statics.build = (attrs: TicketAttrs) => {
     price: attrs.price
   });
 };
+
+ticketSchema.statics.findByEvent = (event: {id: string, version: number}) => {
+  return Ticket.findOne({
+    _id: event.id,
+    version: event.version - 1
+  });
+}
+
 ticketSchema.methods.isReserved = async function () {
   // this === the ticket document that we just called 'isReserved' on
   const existingOrder = await Order.findOne({
